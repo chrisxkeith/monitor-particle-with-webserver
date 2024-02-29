4c01ee65df79236801f46e00179d47be2d980efd
CK <chris.keith@gmail.com>
2024-02-29 11:25:16 -0800

    old HTML code works (well enough)

--- src/main/java/com/example/restservice/RestServiceApplication.java
+++ src/main/java/com/example/restservice/RestServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.ckkeith.monitor.Main;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestServiceApplication.class, args);
+		Main.main(new String[0]);
 	}
 
 }
