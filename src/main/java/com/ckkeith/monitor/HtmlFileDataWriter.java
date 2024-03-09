d8b5720de9ba2a9ebb6dfb723d23b37af703b019
CK <chris.keith@gmail.com>
2024-03-09 10:59:39 -0800

    Data updates. Remove unused HTML-generating code

--- src/main/java/com/ckkeith/monitor/HtmlFileDataWriter.java
+++ src/main/java/com/ckkeith/monitor/HtmlFileDataWriter.java
@@ -1,12 +1,8 @@
 // Please credit chris.keith@gmail.com
 package com.ckkeith.monitor;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
@@ -51,59 +47,6 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 		}
 	}
 
-	private void writeln(FileWriter fw, String s) throws Exception {
-		fw.write(s + System.getProperty("line.separator"));
-	}
-
-	private int colorIndex = 0;
-
-	private String getNextColor() {
-		final String colors[] = {
-				"255, 99, 132, 1",
-				"75, 192, 192, 1",
-				"153, 102, 255, 1",
-				"255, 159, 64, 1",
-				"54, 162, 235, 1",
-				"127, 45, 132, 1",
-				"34, 81, 192, 1",
-				"76, 51, 255, 1",
-				"127, 78, 64, 1",
-				"27, 81, 235, 1"
-		};
-		String nextColor = colors[colorIndex++];
-		if (colorIndex >= colors.length) {
-			colorIndex = 0;
-		}
-		return nextColor;
-	}
-
-	private int addDataForSensor(FileWriter jsonStream, String sensorName) throws Exception {
-		writeln(jsonStream, "\t\t\t\t\"data\" : [");
-		Set<LocalDateTime> keys = sensorData.keySet();
-		Iterator<LocalDateTime> itr = keys.iterator();
-		boolean first = true;
-		int nDataPoints = 0;
-		while (itr.hasNext()) {
-			LocalDateTime timestamp = itr.next();
-			ConcurrentSkipListMap<String, String> entries = sensorData.get(timestamp);
-
-			String val = entries.get(sensorName);
-			if (val != null && !val.isEmpty()) {
-				StringBuilder sb2 = new StringBuilder();
-				if (!first) {
-					sb2.append(",");
-				} else {
-					first = false;
-				}
-				sb2.append("{ \"t\" : \"").append(timestamp).append("\", \"y\" : " + val + "}");
-				writeln(jsonStream, sb2.toString());
-				nDataPoints++;
-			}
-		}
-		writeln(jsonStream, "\t\t\t\t]");
-		return nDataPoints;
-	}
-
 	private String getDisplayNameForSensor(String fullSensorName) {
 		String[] deviceSensorNames = fullSensorName.split(separator);
 		return accountMonitor.runParams.getDisplayNameForSensor(deviceSensorNames[0], 
@@ -126,93 +69,6 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 		}
 		LocalDateTime[] ret = { min, max };
 		return ret;
-	}
-
-	private Integer writeJson(FileWriter jsonStream, String deviceName) throws Exception {
-		Integer nDataPoints = 0;
-		try {
-			colorIndex = 0;
-			writeln(jsonStream, "\t{ \"datasets\" : ");
-			writeln(jsonStream, "\t\t[");
-
-			Iterator<String> sensorIt = sensorNames.keySet().iterator();
-			Boolean firstSensor = true;
-			while (sensorIt.hasNext()) {
-				String sensorName = sensorIt.next();
-				if (((deviceName == null) || sensorName.startsWith(deviceName))) {
-					StringBuilder sb1 = new StringBuilder("\t\t\t");
-					if (firstSensor) {
-						firstSensor = false;
-					} else {
-						sb1.append(" , ");
-					}
-					sb1.append("{ \"label\" : \"");
-					sb1.append(getDisplayNameForSensor(sensorName)).append("\",");
-					writeln(jsonStream, sb1.toString());
-					writeln(jsonStream, "\"lineTension\" : 0,");
-					writeln(jsonStream, "\t\t\t\"borderColor\" : \"rgba(" + getNextColor() + ")\",");
-					writeln(jsonStream, "\t\t\t\"backgroundColor\" : \"rgba(0, 0, 0, 0.0)\",");
-					nDataPoints += addDataForSensor(jsonStream, sensorName);
-					writeln(jsonStream, "\t\t\t}");
-				}
-			}
-		} finally {
-			writeln(jsonStream, "\t\t]");
-			writeln(jsonStream, "\t}");
-		}
-		return nDataPoints;
-	}
-
-	private File findFile(String fn) {
-		File f = new File(fn);
-		if (f.exists()) {
-			return f;
-		}
-		f = new File("monitor-particle-using-api" + File.pathSeparator + fn);
-		if (f.exists()) {
-			return f;
-		}
-		return null;
-	}
-
-	private void appendFromFileToFile(FileWriter htmlStream, String fromFile, String fileName) throws Exception {
-		File f = findFile(fromFile);
-		if (f == null) {
-			return;
-		}
-		try (BufferedReader br = new BufferedReader(new FileReader(f))) {
-			String line;
-			LocalDateTime limits[] = findTimeLimits();
-			while ((line = br.readLine()) != null) {
-				if (line.contains("_fileNameGoesHere_.html")) {
-					String accountPath = Utils.getHomeURLPath(
-							"Documents" + File.separator + "tmp" + File.separator + Utils.getHostName() + File.separator
-									+ Utils.getSafeName(accountMonitor.accountName));
-					line = line.replace("_fileNameGoesHere_", fileName)
-							.replace("_homePathGoesHere_", accountPath)
-							.replace("_writeIntervalGoesHere_",
-									Integer.valueOf(accountMonitor.runParams.htmlWriteIntervalInSeconds).toString());
-				} else if (line.contains("_suggestedTimeMin_")) {
-					line = line.replace("_suggestedTimeMin_", Utils.googleSheetsDateFormat.format(limits[0]));
-				} else if (line.contains("_suggestedTimeMax_")) {
-					LocalDateTime max = LocalDateTime.now();
-					if (max.isBefore(limits[1])) {
-						max = limits[1];
-					}
-					line = line.replace("_suggestedTimeMax_", Utils.googleSheetsDateFormat.format(max));
-				}
-				writeln(htmlStream, line);
-			}
-		}
-	}
-
-	int currentFileNumber = 0;
-
-	int getNextFileNumber(int thisFileNumber) {
-		if (thisFileNumber == 0) {
-			return 1;
-		}
-		return 0;
 	}
 
 	void deleteOldData() {
@@ -323,34 +179,6 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 		}
 	}
 
-	int doWriteOneHtml(String safeFn, File tempFile, String deviceName, int thisFileNumber, String thisFileName)
-				throws Exception {
-		int nDataPoints = 0;
-		FileWriter htmlStream = new FileWriter(tempFile.getCanonicalPath(), false);
-		try {
-			appendFromFileToFile(htmlStream, "src/main/resources/prefix.html",
-					safeFn + String.format("%03d", getNextFileNumber(thisFileNumber)));
-			nDataPoints = writeJson(htmlStream, deviceName);
-			appendFromFileToFile(htmlStream, "src/main/resources/suffix.html", "junk");
-		} finally {
-			htmlStream.close();
-		}
-		File thisFile = new File(thisFileName);
-		try {
-			thisFile.delete();
-		} catch (Exception ex) {
-			Utils.logToConsole(
-					"thisFile.delete() failed : " + ex.getMessage() + " " + ex.getClass().getName() + ", continuing");
-		}
-		try {
-			Files.move(tempFile.toPath(), thisFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-		} catch (Exception ex) {
-			Utils.logToConsole(
-					"Files.move() failed : " + ex.getMessage() + " " + ex.getClass().getName() + ", continuing");
-		}
-		return nDataPoints;
-	}
-
 	String getHTMLFileName(String deviceName) throws Exception {
 		String safeFn;
 		if (deviceName == null) {
@@ -363,38 +191,6 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 		return safeFn;
 	}
 
-	void writeOneHtml(String deviceName, int thisFileNumber) {
-		String thisFileName = "not yet specified";
-		int nDataPoints = 0;
-		try {
-			deleteOldData();
-			String safeFn;
-			safeFn = getHTMLFileName(deviceName);
-			String fileName = Utils.getLogFileName(accountMonitor.accountName, safeFn + "NNN.html");
-			thisFileName = fileName.replace("NNN", String.format("%03d", thisFileNumber));
-			String dir = new File(fileName).getParent();
-			File tempFile = File.createTempFile("tmp", ".html", new File(dir));
-			try {
-				nDataPoints = doWriteOneHtml(safeFn, tempFile, deviceName, thisFileNumber, thisFileName);
-			} finally {
-				tempFile.delete();
-			}
-		} catch (Exception e) {
-			Utils.logToConsole("FAILED to write : " + thisFileName + " : # data points : "
-					+Integer.valueOf(nDataPoints).toString() + " : " + e.getMessage());
-			e.printStackTrace();
-			// If there's any failure, continue and write the next file at the appropriate time.
-		}
-	}
-
-	void writeHtml() throws Exception {
-		synchronized (this) {
-			int thisFileNumber = currentFileNumber;
-			writeOneHtml(null, thisFileNumber);
-			currentFileNumber = getNextFileNumber(currentFileNumber);
-		}
-	}
-	
 	void fillEmpty() {
 		ConcurrentSkipListMap<String, String> prevEntries = new ConcurrentSkipListMap<String, String>();
 		Iterator<LocalDateTime> sensorDataIt = sensorData.keySet().iterator();
@@ -424,7 +220,6 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 			Utils.logToConsole("JSON: http://localhost:8080/sensordata");
 			while (true) {
 				fillEmpty();
-				writeHtml();
 				this.writeCSV();
 				Thread.sleep(accountMonitor.runParams.htmlWriteIntervalInSeconds * 1000);
 			}
@@ -437,9 +232,23 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 	private record Datapoint(String t, Integer y) {}
 	private record Dataset(String label, Integer lineTension, String borderColor, String backgroundColor, Object[] data) {}
 	
+	private final String colors[] = {
+		"255, 99, 132, 1",
+		"75, 192, 192, 1",
+		"153, 102, 255, 1",
+		"255, 159, 64, 1",
+		"54, 162, 235, 1",
+		"127, 45, 132, 1",
+		"34, 81, 192, 1",
+		"76, 51, 255, 1",
+		"127, 78, 64, 1",
+		"27, 81, 235, 1"
+	};
+
 	private List<Dataset> datasets() {
 		List<Dataset> datasetArray = new ArrayList<Dataset>();
 		Iterator<String> sensorIt = sensorNames.keySet().iterator();
+		int colorIndex = 0;
 		while (sensorIt.hasNext()) {
 			String sensorName = sensorIt.next();
 			Set<LocalDateTime> keys = sensorData.keySet();
@@ -454,8 +263,11 @@ sensorNames.put(fullSensorName, sensorDataPoint.sensorName);
 				}
 			}
 			datasetArray.add(new Dataset(getDisplayNameForSensor(sensorName), 0, 
-								"rgba(" + getNextColor() + ")", "rgba(0, 0, 0, 0.0)",
+								"rgba(" + colors[colorIndex++] + ")", "rgba(0, 0, 0, 0.0)",
 								datapointArray.toArray()));
+			if (colorIndex >= colors.length) {
+				colorIndex = 0;
+			}
 		}
 		return datasetArray;
 	}
