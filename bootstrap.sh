eeae9fe126ad2e1b83fefffab4da7333cd0c1e8f
Christopher Keith <chris.keith@gmail.com>
2025-08-31 18:54:38 -0700

    Remove incorrect bootstrap command

--- bootstrap.sh
+++ bootstrap.sh
@@ -36,8 +36,7 @@ rm -rf JParticle/									                    ; if [ $? -ne 0 ] ; then exit -6 ;
 cd ~/Documents/Github/${repo}	  					                    ; if [ $? -ne 0 ] ; then exit -6 ; fi
 mkdir -p src/main/resources/com/ckkeith/monitor/	  					; if [ $? -ne 0 ] ; then exit -6 ; fi
 
-cp src/main/resources/com/ckkeith/monitor/runparams.json ~/Documents/tmp/${HOSTNAME}/${particleAccountName}/
-echo "Edit the runparams.json copy as necessary."
+echo "Edit src/main/resources/com/ckkeith/monitor/runparams.json as necessary."
 echo "Open src/main/java/com/example/restservice/RestServiceApplication.java"
 echo "Run from within VSC."
 echo "Verify using http://localhost:8080/sensordata"
